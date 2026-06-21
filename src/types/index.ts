08c9021be7e8d3971e12c7981b63267432a67d18
Abdulhadi <abdulmuminhadi76@gmail.com>
2026-06-21 06:27:57 +0100

    Add files via upload

--- src/types/index.ts
+++ src/types/index.ts
@@ -22,3 +22,49 @@ export interface SignupProfileInput {
   dob: string
   connectedPlatform: string | null
 }
+
+export interface MockUser {
+  id: number
+  username: string
+  displayName: string
+  bio: string
+  xp: number
+  level: number
+  streak: number
+  followers: number
+  following: number
+  friends: number
+  color: string
+  joinDate: string
+}
+
+export interface GameDetail {
+  score: string
+  duration: string
+  xpEarned: number
+  players: string[]
+  map: string
+}
+
+export interface StudioDetail {
+  caption: string
+  likes: number
+  comments: number
+  shares: number
+}
+
+export interface AchievementDetail {
+  name: string
+  desc: string
+  xpEarned: number
+  rarity: string
+}
+
+export interface FeedItem {
+  id: number
+  type: 'game' | 'studio' | 'achievement'
+  time: string
+  title: string
+  sub: string
+  detail: GameDetail | StudioDetail | AchievementDetail
+}
