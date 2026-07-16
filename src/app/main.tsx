9c610750ae325260a1102afba1ee86356efd62aa
Victor <onyebobo1234@gmail.com>
2026-07-16 22:01:50 +0100

    Add files via upload

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { HelmetProvider } from 'react-helmet-async'
 import App from './App'
 import ErrorBoundary from '../shared/components/ErrorBoundary'
 import { AuthProvider } from '../features/auth/useAuth'
@@ -9,11 +10,13 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ErrorBoundary>
-      <AuthProvider>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </AuthProvider>
+      <HelmetProvider>
+        <AuthProvider>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </AuthProvider>
+      </HelmetProvider>
     </ErrorBoundary>
   </React.StrictMode>
 )
